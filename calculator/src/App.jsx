e52ce133ab53149b64c73307e5201df4e9d26aa5
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-28 09:27:54 +0200

    Output screen, lining initial CSS

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,50 +1,124 @@
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
-  
-
-
-  return(
+  return (
     <>
-    <div>
-    <h1>Calculator in React</h1>
-    <br/>
-    <br/>
-    <table class="table-sm table-borderless">
-  <tbody>
-    <tr>
-      <td><button class="btn btn-primary" type="button">7</button></td>
-      <td><button class="btn btn-primary" type="button">8</button></td>
-      <td><button class="btn btn-primary" type="button">9</button></td>
-      <td><button class="btn btn-danger" type="button">C</button></td>
-    </tr>
-    <tr>
-      <td><button class="btn btn-primary" type="button">4</button></td>
-      <td><button class="btn btn-primary" type="button">5</button></td>
-      <td><button class="btn btn-primary" type="button">6</button></td>
-      <td><button class="btn btn-danger" type="button">&#60;-</button></td>
-    </tr>
-    <tr>
-    <td><button class="btn btn-primary" type="button">1</button></td>
-    <td><button class="btn btn-primary" type="button">2</button></td>
-    <td><button class="btn btn-primary" type="button">3</button></td>
-    <td><button class="btn btn-info" type="button">/</button></td>
-    </tr>
-    <tr>
-      <td><button class="btn btn-info" type="button">+</button></td>
-      <td><button class="btn btn-primary" type="button">0</button></td>
-      <td><button class="btn btn-info" type="button">-</button></td>
-      <td><button class="btn btn-info" type="button">*</button></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td><button class="btn btn-primary" type="button">.</button></td>
-      <td><button class="btn btn-success" type="button">=</button></td>
-    </tr>
-  </tbody>
-</table>
-    </div>
+      <div>
+        <h1>Calculator in React</h1>
+        <br />
+        <br />
+        <table class="table-sm table-borderless">
+          <div>
+            <div style={{backgroundColor:"gray"}}>
+            <span></span>
+            </div>
+            <tbody>
+              <tr>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    7
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    8
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    9
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger" type="button" style={{width:40}}>
+                    C
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    4
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    5
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    6
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger" type="button" style={{width:40}}>
+                    &#60;-
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    1
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    2
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    3
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-info" type="button" style={{width:40}}>
+                    /
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button class="btn btn-info" type="button">
+                    +
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    0
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-info" type="button">
+                    -
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-info" type="button" style={{width:40}}>
+                    *
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td>
+                  <button class="btn btn-primary" type="button">
+                    .
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-success" type="button" style={{width:40}}>
+                    =
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </div>
+        </table>
+      </div>
     </>
-  )
+  );
 }
